59ff82488314b0c03d7408de73852bd0ef491c55
Shantanu Badmore <badmoreshantanu@gmail.com>
2021-10-18 11:49:45 +0530

    Test String Calculator done

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -18,6 +18,64 @@ class StringCalculatorShould {
         assertEquals(1, stringCalculator.add("1"));
     }
 
-		
 
+    @Test
+    void string_with_two_number(){
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("1,2"));
+    }
+
+    @Test
+    void string_with_many_numbers(){
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(16, stringCalculator.add("1,2,5,8"));
+    }
+
+    @Test
+    void string_with_new_line_delimeter(){
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(6, stringCalculator.add("1,2\n3"));
+    }
+
+    @Test
+    void string_supporting_different_delimeter(){
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("//;\n1;2"));
+    }
+
+    @Test
+    void numbers_bigger_than_thousand(){
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(2, stringCalculator.add("2+1001"));
+    }
+
+    @Test
+    void delimter_with_any_length(){
+        StringCalculator stringCalculator= new StringCalculator();
+        assertEquals(6, stringCalculator.add("//[***]\\n1***2***3"));
+    }
+
+    @Test
+    void multiple_delimeter(){
+        StringCalculator stringCalculator= new StringCalculator();
+        assertEquals(6, stringCalculator.add("//[*][%]\\n1*2%3"));
+    }
+
+    @Test
+    void multiple_delimeters_with_length_longer_than_one_char(){
+        StringCalculator stringCalculator= new StringCalculator();
+        assertEquals(6, stringCalculator.add("//[**][%%]\\n1**2%%3"));
+    }
+
+    @Test
+    void count_no_of_times_add_called(){
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(10, stringCalculator.GetCalledCount());
+    }
+
+    @Test
+    void string_with_negative_numbers(){
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(-3, stringCalculator.add("1-3"));
+    }
 }
